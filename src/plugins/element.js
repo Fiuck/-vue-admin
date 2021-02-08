fd9e03fe5896a8ffb370df62d1c58b7f8b2297a3
zhaowx <643948989@qq.com>
2021-02-08 09:08:03 +0800

    主页面布局

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
-import { Button,Message } from 'element-ui'
+import { Button,Message,Container,Header,Aside,Main } from 'element-ui'
+import { Menu,Submenu,MenuItemGroup,MenuItem} from 'element-ui'
 
 Vue.use(Button)
 /**
  * Message和MessageBox两个组件比较特殊，在import引入之后不需要Vue.use
  */
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+
 Vue.prototype.$message = Message;
